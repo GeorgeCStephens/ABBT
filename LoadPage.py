e24d2850eb76f920be6b732ab5d5bd2e9cd6b150
iggy <iggy@pop-os.home>
2024-09-18 17:12:06 +0100

    Implemented code to construct dictionary of time, court, xPathToBook for the book button

--- LoadPage.py
+++ LoadPage.py
@@ -26,26 +26,24 @@ print("     Done")
 
 #Check all XPaths and store the ones we want
 print("Building List Of Open Slots...")
-allAvailableSlots=[]
-allAvailableSlotsXPath=[]
+allBookableSlots=[]
+allBookableSlotsXPath=[]
 for xPath in allPossibleSlots:
     try:
         validSlot=driver.find_element(By.XPATH, xPath)
-        allAvailableSlots.append(validSlot)
-        allAvailableSlotsXPath.append(xPath)
+        allBookableSlots.append(validSlot)
+        allBookableSlotsXPath.append(xPath)
     except:
         pass
 print("     Done")
 
 #Get the time of each slot
 print("Gathering Times Of Available Slots...")
-for openSlotXPath in allAvailableSlotsXPath:
+allTimeSlots=[]
+for openSlotXPath in allBookableSlotsXPath:
     timeXPath=str(openSlotXPath).removesuffix("app-button/button/span/span")+"p"
     timeSlot=driver.find_element(By.XPATH, timeXPath)
-    print(timeSlot)
+    courtXPath=str(openSlotXPath).removesuffix("app-button/button/span/span")+"h3"
+    courtSlot=driver.find_element(By.XPATH, courtXPath)
+    allTimeSlots.append({"time":timeSlot.text, "court":courtSlot.text,"xPathToBook":str(openSlotXPath)})
 print("     Done")
-
-#allOpenSlots=driver.find_elements(By.XPATH, "//*[contains(text(), 'Book now')]")
-
-#/html/body/app-root/div/main/app-language/ng-component/div/div[1]/section/ng-component/div/form/app-activity-calendar-timetable/div/app-overflow-carousel/div/div/div/app-overflow-carousel-slide[1]/div/div/ol/li[2]/app-activity-calendar-timetable-slot/div/div
-#/html/body/app-root/div/main/app-language/ng-component/div/div[1]/section/ng-component/div/form/app-activity-calendar-timetable/div/app-overflow-carousel/div/div/div/app-overflow-carousel-slide[1]/div/div/ol/li[3]/app-activity-calendar-timetable-slot/div/app-button/button/span/span
